bf0af0804c0fd5a643dd06ba7971257200552ce2
Aziz Amerul Faozi <cahalapisan@gmail.com>
2018-05-06 19:46:26 +0700

    oke dunkin donit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DEFAULT_QUERY ='redux';
+const PATH_BASE='https://hn.algolia.com/api/v1';
+const PATH_SEARCH='/search';
+const PARAM_SEARCH='query=';
 
 const list = [
 {
@@ -78,13 +82,28 @@ class App extends Component {
 	constructor(props){
 		super(props);
 		this.state={
-			list: list,
-			searchTerm:'',
+			//list: list,
+			result: null,
+			searchTerm: DEFAULT_QUERY,
 		};
+		this.setSearchTopStories=this.setSearchTopStories.bind(this);
 		this.onSearchChange=this.onSearchChange.bind(this);
 		this.onDismiss=this.onDismiss.bind(this);
 
 	}
+
+	setSearchTopStories(result){
+		this.setState({result});
+	}
+
+	componentDidMount(){
+		const{searchTerm}=this.state;
+		fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+		.then(response=> response.json())
+		.then(result=>this.setSearchTopStories(result))
+		.catch(error=>error);
+	}
+
 	onSearchChange(event)
 	{
 		this.setState({searchTerm: event.target.value});
@@ -92,12 +111,20 @@ class App extends Component {
 
 	onDismiss(id){
 		const isNotId=item=> item.objectID!==id;
-		const updatedList=this.state.list.filter(isNotId);
-		this.setState({list: updatedList});
+		const updatedHits=this.state.result.hits.filter(isNotId);
+		//const updatedList=this.state.list.filter(isNotId);
+		this.setState({
+			//list: updatedList
+			result: Object.assign({}, this.state.result, {hits:updatedHits})
+		});
+
 	}
 
 	render() {
-		const{searchTerm, list}=this.state;
+		const{searchTerm, result}=this.state;
+		if (!result){
+			return null;
+		}
 		return (
 			<div className="page">
 				<div className="interactions">
@@ -110,7 +137,7 @@ class App extends Component {
 				</div>
 
 				<Table 
-					list={list}
+					list={result.hits}
 					pattern={searchTerm}
 					onDismiss={this.onDismiss}
 				/>
